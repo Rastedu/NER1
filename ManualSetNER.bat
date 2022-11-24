37dad018adcfb821e136f2cc6f6f8b88c16528b8
Nikita <velichko_nikitka@mail.ru>
2022-11-24 23:27:41 +0500

    Delete ManualSetNER.bat

--- ManualSetNER.bat
+++
@@ -1,7 +0,0 @@
-python -m venv nerauto
-nerauto\Scripts\activate
-pip install deeppavlov
-pip install pandas jupyter
-python -m pip install --upgrade pip
-python -m deeppavlov download ner_ontonotes_bert
-python -m deeppavlov riseapi ner_ontonotes_bert -p 5005
